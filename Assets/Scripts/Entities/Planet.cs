4683a819587abbab7fa21a7f278834d1b4ed2647
DragunDev <plarisanmarc@gmail.com>
2025-05-21 21:22:05 +0800

    Implement planet despawning

--- Assets/Scripts/Entities/Planet.cs
+++ Assets/Scripts/Entities/Planet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public sealed class Planet : MonoBehaviour
 {
+    private const string PLANET_DESPAWN_TAG = "PlanetDespawnArea";
+
     [SerializeField] float speed = 1f;
 
     private void Start()
@@ -19,5 +21,13 @@ public sealed class Planet : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(PLANET_DESPAWN_TAG))
+        {
+            Destroy(gameObject);
+        }
     }
 }
