369245433a95cc572a47d72530c7b2b3e9a3a2b1
DragunDev <plarisanmarc@gmail.com>
2025-05-24 18:57:20 +0800

    Implement increase in planet speed for difficulty

--- Assets/Scripts/Entities/Planet.cs
+++ Assets/Scripts/Entities/Planet.cs
@@ -7,13 +7,26 @@ public sealed class Planet : MonoBehaviour
     private const string PLANET_DESPAWN_TAG = "PlanetDespawnArea";
     private const int BASE_SCORE_GAIN = 5;
 
+    private const float BASE_DIFFICULTY_INCREASE_IN_SPEED = 0.05f;
+    private const float BASE_MIN_SPEED = 0.5f;
+    private const float BASE_MAX_SPEED = 3f;
+
     [SerializeField] float speed = 1f;
 
     private GameState gameState;
+    private DifficultyScaler difficultyScaler;
+
+    private void Awake()
+    {
+        difficultyScaler = GameObject.Find(Constants.SCRIPTS_GAME_OBJECT).GetComponent<DifficultyScaler>();
+    }
 
     private void Start()
     {
         gameState = GameObject.Find(Constants.SCRIPTS_GAME_OBJECT).GetComponent<GameState>();
+
+        float speedIncrease = difficultyScaler.GetDifficultyLevel() * BASE_DIFFICULTY_INCREASE_IN_SPEED;
+        speed = Random.Range(BASE_MIN_SPEED + speedIncrease, BASE_MAX_SPEED + speedIncrease);
     }
 
     private void Update()
